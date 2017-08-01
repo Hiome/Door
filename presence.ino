afe4267ea709a6e2cf1be936ab3c80e7787d56b9
Neil Gupta <ngupta@instructure.com>
2017-07-31 23:31:02 -0500

    refactor for cleaner code

--- presence.ino
+++ presence.ino
@@ -17,6 +17,11 @@
 #define PIR 14   //D5
 #define BLUE_LED 2
 
+/* Constants */
+#define SENSOR_TIMEOUT 50000
+#define SENSOR_ERROR 0x2
+#define SENSOR_DELAY 10
+
 WiFiClient esp_client;
 PubSubClient mqtt_client(esp_client);
 
@@ -32,7 +37,7 @@ static const char* mac_address() {
 static const char* client_name = mac_address();
 
 /* Generate MQTT topic with the MAC address.
- *  Topic name will always be "smarter/presence/MACADDRESS"
+ *  Topic name will always be "smarter/presence/data/MACADDRESS"
  */
 static const char* mqtt_topic() {
   static char topic[40];
@@ -56,54 +61,20 @@ void setup() {
   calibrate();
 }
 
-void calibrate() {
-  // give the sensors ~30 seconds to calibrate
-  for(int i = 0; i < 600; i++){
-    read_ultrasonic_sensor();
-    delay(50);
-  }
-}
-
-int read_ultrasonic_sensor() {
-  digitalWrite(TRIG, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
-  long duration = pulseIn(ECHO, HIGH);
-
-  if (duration > 20000) {
-    // unreasonable response (more than 3 meters away),
-    // ignore it so we don't mess up our average
-    return LOW;
-  }
-
-  static long avg = duration;
-
-  static const int POWER = 256;
-  static const int SLOW_ALPHA = 26; // 10% of 256
-  static const int FAST_ALPHA = 78; // 30% of 256
-
-  if (duration < ((POWER - SLOW_ALPHA)*avg/POWER)) {
-    // activity detected: duration is within 90% of our average,
-    // weight it 10% in our running average and return HIGH
-    avg = (SLOW_ALPHA * duration + (POWER - SLOW_ALPHA) * avg) / POWER;
-    return HIGH;
-  } else {
-    // no activity detected: duration is more than 90% of our average,
-    // weight it 30% in our running average and return LOW
-    avg = (FAST_ALPHA * duration + (POWER - FAST_ALPHA) * avg) / POWER;
-    return LOW;
-  }
-}
-
 /* Connect to wifi */
 void setup_wifi() {
+  Serial.print("Connecting to ");
+  Serial.print(wifi_ssid);
+
   WiFi.begin(wifi_ssid, wifi_password);
 
   while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(" . ");
     delay(500);
   }
+
+  Serial.print(WiFi.localIP());
+  Serial.println(" connected.");
 }
 
 /* Connect to MQTT broker */
@@ -116,13 +87,62 @@ void setup_mqtt() {
   static char lifecycle_topic[45];
   sprintf(lifecycle_topic, "smarter/presence/lifecycle/%s", client_name);
 
+  Serial.print("Connecting to ");
+  Serial.print(mqtt_host);
   // loop until reconnected
   while (!mqtt_client.connected()) {
+    Serial.print(" . ");
     if (mqtt_client.connect(client_name, lifecycle_topic, 0, true, "disconnected")) {
+      Serial.print(client_name);
+      Serial.println(" connected.");
       mqtt_client.publish(lifecycle_topic, "connected", true);
     } else {
       delay(1000);
     }
+  }
+}
+
+void calibrate() {
+  Serial.print("Calibrating for ~30 seconds... ");
+  // give the sensors ~30 seconds to calibrate
+  for(int i = 0; i < 600; i++){
+    read_ultrasonic_sensor();
+    delay(50);
+  }
+  Serial.println("done.");
+}
+
+uint8_t read_ultrasonic_sensor() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  long duration = pulseIn(ECHO, HIGH);
+
+  if (duration > SENSOR_TIMEOUT) {
+    // unreasonable response (more than 3 meters away),
+    // ignore it so we don't mess up our average.
+    return SENSOR_ERROR;
+  }
+
+  static long avg = duration;
+
+  static const int POWER = 256;
+  static const int PADDING = 8; // 3% of 256
+  static const int SLOW_ALPHA = 26; // 10% of 256
+  static const int FAST_ALPHA = 78; // 30% of 256
+
+  if (duration < ((POWER - PADDING)*avg/POWER)) {
+    // activity detected: duration is within 90% of our average,
+    // weight it 10% in our running average and return HIGH
+    avg = (SLOW_ALPHA * duration + (POWER - SLOW_ALPHA) * avg) / POWER;
+    return HIGH;
+  } else {
+    // no activity detected: duration is more than 90% of our average,
+    // weight it 30% in our running average and return LOW
+    avg = (FAST_ALPHA * duration + (POWER - FAST_ALPHA) * avg) / POWER;
+    return LOW;
   }
 }
 
@@ -131,20 +151,36 @@ void loop() {
     setup_mqtt();
   }
 
-  if (read_ultrasonic_sensor() == HIGH) {
-    if (digitalRead(PIR) == HIGH) {
-      digitalWrite(BLUE_LED, LOW);
-      mqtt_client.publish(data_topic, "exited", true);
-    } else {
-      digitalWrite(BLUE_LED, HIGH);
-      mqtt_client.publish(data_topic, "entered", true);
+  static boolean was_active = false;
+  uint8_t sonar = read_ultrasonic_sensor();
+  int pir_val = digitalRead(PIR);
+  if (pir_val == HIGH) {
+    Serial.print("PIR: ");
+    Serial.println(pir_val);
+    Serial.print("Sonar: ");
+    Serial.println(sonar);
+  }
+
+  if (sonar == HIGH) {
+    if (!was_active) {
+      if (pir_val == HIGH) {
+        mqtt_client.publish(data_topic, "exited", true);
+        digitalWrite(BUILTIN_LED, LOW);
+        digitalWrite(BLUE_LED, HIGH);
+      } else {
+        mqtt_client.publish(data_topic, "entered", true);
+        digitalWrite(BLUE_LED, LOW);
+        digitalWrite(BUILTIN_LED, HIGH);
+      }
     }
-    digitalWrite(BUILTIN_LED, LOW);
-  } else {
+    was_active = true;
+  } else if (sonar == LOW) {
+    was_active = false;
     digitalWrite(BUILTIN_LED, HIGH);
     digitalWrite(BLUE_LED, HIGH);
   }
 
-  delay(10); // give the ultrasonic sound waves some time to dissipate
+  // give the sound waves some time to dissipate
+  delay(SENSOR_DELAY);
 }
 
