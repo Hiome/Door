4cab8e6789a0191cf5494efe9f41d1f8fd95e0da
Neil Gupta <ngupta@instructure.com>
2017-08-08 01:07:00 -0500

    Rewrite for vl53l0x chipset

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# SmarterPresence
+# SmarterDoor
 
-Presence detector that uses a dual-technology approach to combine multiple sensor
-types for a more accurate presence reading.
+Detect when somebody enters or exits a room.
 
-This code is written to run on an ESP8266 development board with an ultrasonic
-sensor and PIR motion sensor. It assumes you have an MQTT broker on your network
-to broadcast enter/exit events to.
+Uses VL53L0X mini-LIDAR chips to detect movement and calculate direction. It runs on an ESP8266 board to communicate over wifi to an MQTT broker.
--- presence.ino
+++ presence.ino
@@ -1,5 +1,5 @@
 /*
- *  Check ultrasonic and PIR sensors repeatedly for human presence.
+ *  Check VL53L0X sensors repeatedly for human presence and direction of movement.
  *  Reads sensor data and streams any changes to an MQTT broker.
  *  
  *  Copyright 2017 Neil Gupta
@@ -7,23 +7,25 @@
  *  
  */
 
+#include <Wire.h>
+#include <VL53L0X.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include "secrets.h"
 
 /* Pin Connections */
-#define TRIG 13  //D7
-#define ECHO 15  //D8
-#define PIR 14   //D5
-#define BLUE_LED 2
+#define xshut1 14 // D6
+#define xshut2 12 // D5
 
-/* Constants */
-#define SENSOR_TIMEOUT 50000
-#define SENSOR_ERROR 0x2
-#define SENSOR_DELAY 10
+#define SENSOR_LOW 0
+#define SENSOR_HIGH 1
+#define SENSOR_ERROR 2
 
 WiFiClient esp_client;
 PubSubClient mqtt_client(esp_client);
+
+VL53L0X sensor1;
+VL53L0X sensor2;
 
 /* Get MAC address as a char array */
 static const char* mac_address() {
@@ -48,15 +50,36 @@ static const char* mqtt_topic() {
 static const char* data_topic = mqtt_topic();
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECHO, INPUT);
-  pinMode(PIR, INPUT);
-  pinMode(BUILTIN_LED, OUTPUT);
-  pinMode(BLUE_LED, OUTPUT);
+  pinMode(xshut1, OUTPUT);
+  pinMode(xshut2, OUTPUT);
+  digitalWrite(xshut1, LOW);
+  digitalWrite(xshut2, LOW);
+
+  Serial.begin (9600);
 
   setup_wifi();
   mqtt_client.setServer(mqtt_host, mqtt_port);
+
+  Wire.begin(4, 5);
+
+  Serial.println("Initializing Sensor 1...");
+  pinMode(xshut1, INPUT);
+  delay(150);
+  sensor1.init();
+  delay(100);
+  sensor1.setAddress((uint8_t)22);
+  
+  Serial.println("Initializing Sensor 2...");
+  pinMode(xshut2, INPUT);
+  delay(150);
+  sensor2.init();
+  delay(100);
+  sensor2.setAddress((uint8_t)25);
+
+  sensor1.setMeasurementTimingBudget(20000);
+  sensor1.startContinuous();
+  sensor2.setMeasurementTimingBudget(20000);
+  sensor2.startContinuous();
 
   calibrate();
 }
@@ -89,6 +112,7 @@ void setup_mqtt() {
 
   Serial.print("Connecting to ");
   Serial.print(mqtt_host);
+
   // loop until reconnected
   while (!mqtt_client.connected()) {
     Serial.print(" . ");
@@ -103,47 +127,58 @@ void setup_mqtt() {
 }
 
 void calibrate() {
-  Serial.print("Calibrating for ~30 seconds... ");
-  // give the sensors ~30 seconds to calibrate
-  for(int i = 0; i < 600; i++){
-    read_ultrasonic_sensor();
-    delay(50);
+  Serial.print("Calibrating for ~10 seconds... ");
+  for(int i = 0; i < 500; i++){
+    read_sensor1();
+    read_sensor2();
+    delay(1); // needed to reset watchdog timer
   }
   Serial.println("done.");
 }
 
-uint8_t read_ultrasonic_sensor() {
-  digitalWrite(TRIG, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
-  long duration = pulseIn(ECHO, HIGH);
+static const int POWER = 256;
+static const int PADDING = 18; // 7% of 256
+static const int ALPHA = 38; // 15% of 256
 
-  if (duration > SENSOR_TIMEOUT) {
-    // unreasonable response (more than 3 meters away),
-    // ignore it so we don't mess up our average.
+static uint16_t sensor1_range = 0;
+static uint16_t sensor2_range = 0;
+
+uint8_t read_sensor1() {
+  uint16_t range = sensor1.readRangeContinuousMillimeters();
+  if (range > 7000) {
     return SENSOR_ERROR;
   }
 
-  static long avg = duration;
-
-  static const int POWER = 256;
-  static const int PADDING = 8; // 3% of 256
-  static const int SLOW_ALPHA = 26; // 10% of 256
-  static const int FAST_ALPHA = 78; // 30% of 256
-
-  if (duration < ((POWER - PADDING)*avg/POWER)) {
-    // activity detected: duration is within 90% of our average,
-    // weight it 10% in our running average and return HIGH
-    avg = (SLOW_ALPHA * duration + (POWER - SLOW_ALPHA) * avg) / POWER;
-    return HIGH;
-  } else {
-    // no activity detected: duration is more than 90% of our average,
-    // weight it 30% in our running average and return LOW
-    avg = (FAST_ALPHA * duration + (POWER - FAST_ALPHA) * avg) / POWER;
-    return LOW;
+  sensor1_range = range;
+  static uint16_t avg = range;
+  uint8_t motion = range < ((POWER - PADDING)*avg/POWER) ? SENSOR_HIGH : SENSOR_LOW;
+  if (motion == SENSOR_HIGH) {
+    Serial.print("sensor1: ");
+    Serial.print(range);
+    Serial.print("/");
+    Serial.println(avg);
   }
+  avg = (ALPHA * range + (POWER - ALPHA) * avg) / POWER;
+  return motion;
+}
+
+uint8_t read_sensor2() {
+  uint16_t range = sensor2.readRangeContinuousMillimeters();
+  if (range > 7000) {
+    return SENSOR_ERROR;
+  }
+
+  sensor2_range = range;
+  static uint16_t avg = range;
+  uint8_t motion = range < ((POWER - PADDING)*avg/POWER) ? SENSOR_HIGH : SENSOR_LOW;
+  if (motion == SENSOR_HIGH) {
+    Serial.print("--- sensor2: ");
+    Serial.print(range);
+    Serial.print("/");
+    Serial.println(avg);
+  }
+  avg = (ALPHA * range + (POWER - ALPHA) * avg) / POWER;
+  return motion;
 }
 
 void loop() {
@@ -151,36 +186,73 @@ void loop() {
     setup_mqtt();
   }
 
-  static boolean was_active = false;
-  uint8_t sonar = read_ultrasonic_sensor();
-  int pir_val = digitalRead(PIR);
-  if (pir_val == HIGH) {
-    Serial.print("PIR: ");
-    Serial.println(pir_val);
-    Serial.print("Sonar: ");
-    Serial.println(sonar);
-  }
+  static uint8_t _start = 0;
+  static uint8_t _end = 0;
 
-  if (sonar == HIGH) {
-    if (!was_active) {
-      if (pir_val == HIGH) {
-        mqtt_client.publish(data_topic, "exited", true);
-        digitalWrite(BUILTIN_LED, LOW);
-        digitalWrite(BLUE_LED, HIGH);
+  uint8_t s1 = read_sensor1();
+  uint8_t s2 = read_sensor2();
+
+  if (s1 != SENSOR_ERROR && s2 != SENSOR_ERROR) {
+    // Neither of the readings resulted in an error. Therefore,
+    // s1 and s2 are each either 1 or 0, depending on activity, so
+    // diff can be either 1 (1 - 0), 0 (1 - 1, 0 - 0), or -1 (0 - 1)
+    int diff = s1 - s2;
+
+    if (diff == 0) {
+      // either there is no activity or user is in the middle of both
+      // sensors, so we know nothing about directional intent anyway.
+      if (s1 == SENSOR_LOW && s2 == SENSOR_LOW) { // there's no activity
+        if (_start > 0 && _end > 0) { // activity just ended
+          // _start and _end can be either 1 or 2 (see below), so
+          // dir can be either 1 (2 - 1), 0 (1 - 1, 2 - 2), or -1 (1 - 2).
+          // 0 means the user walked in and out on the same side, don't care.
+          int dir = _start - _end;
+          if (dir == 1) {
+            // moved from sensor 2 to sensor 1
+            mqtt_client.publish(data_topic, "2-1");
+            Serial.println("---- 2-1 ----");
+          } else if (dir == -1) {
+            // moved from sensor 1 to sensor 2
+            mqtt_client.publish(data_topic, "1-2");
+            Serial.println("---- 1-2 ----");
+          }
+        }
+  
+        if (_start != 0 || _end != 0) {
+          Serial.println("all clear!");
+          Serial.println();
+          Serial.println();
+        }
+        // reset values
+        _start = 0;
+        _end = 0;
       } else {
-        mqtt_client.publish(data_topic, "entered", true);
-        digitalWrite(BLUE_LED, LOW);
-        digitalWrite(BUILTIN_LED, HIGH);
+        // we are in the middle of both lasers, try to guess what direction we're moving
+        int closer_sensor = (sensor1_range < sensor2_range ? 1 : 2);
+        if (_start == 0) {
+          // somehow we didn't pick up a starting side, let's guess it
+          _start = closer_sensor;
+          Serial.print("guessing start ");
+          Serial.println(_start);
+        } else {
+          // guess ending direction, in case we don't pick up the ending side
+          _end = closer_sensor;
+          Serial.print("guessing direction ");
+          Serial.println(_end);
+        }
+      }
+    } else {
+      // there is activity on one side or the other
+      if (_start == 0) {
+        // activity is just starting.
+        // diff can be either 1 or -1. Positive diff implies
+        // sensor 1 is the cause of activity, else sensor 2
+        _start = (diff == 1 ? 1 : 2);
+      } else {
+        // activity is ongoing, track where it might end.
+        _end = (diff == 1 ? 1 : 2);
       }
     }
-    was_active = true;
-  } else if (sonar == LOW) {
-    was_active = false;
-    digitalWrite(BUILTIN_LED, HIGH);
-    digitalWrite(BLUE_LED, HIGH);
   }
-
-  // give the sound waves some time to dissipate
-  delay(SENSOR_DELAY);
 }
 
