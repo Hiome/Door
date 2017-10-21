20babf82f05bc2a912f2ae17303c847091fe31c3
Neil Gupta <ngupta@instructure.com>
2017-10-21 11:44:56 -0500

    port to rfm69 radio

--- presence.ino
+++ presence.ino
@@ -1,6 +1,6 @@
 /*
  *  Check VL53L0X sensors repeatedly for human presence and direction of movement.
- *  Reads sensor data and streams any changes to an MQTT broker.
+ *  Reads sensor data and streams any changes over an RFM69 radio.
  *  
  *  Copyright 2017 Neil Gupta
  *  All rights reserved.
@@ -9,13 +9,15 @@
 
 #include <Wire.h>
 #include <VL53L0X.h>
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-#include "secrets.h"
+
+#include <RFM69_ATC.h>
+#include <SPI.h>
+#include <LowPower.h>
+#include <SPIFlash.h>
 
 /* Pin Connections */
-#define xshut1 14 // D6
-#define xshut2 12 // D5
+#define xshut1 4
+#define xshut2 5
 
 #define SENSOR_LOW 0
 #define SENSOR_HIGH 1
@@ -23,47 +25,41 @@
 
 #define TIMEOUT_THRESHOLD 7000
 #define CONFIDENCE_THRESHOLD 4
+#define SERIAL_BAUD    115200
 
-WiFiClient esp_client;
-PubSubClient mqtt_client(esp_client);
+#define NODEID        2   //unique for each node on same network
+#define NETWORKID     27  //the same on all nodes that talk to each other
+#define GATEWAYID     1
+#define FREQUENCY     RF69_915MHZ
+#define ENCRYPTKEY    "smarterisbetters" //exactly the same 16 characters/bytes on all nodes!
+#define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROL
+#define ATC_RSSI      -90
+#define ACK_TIME      30  // max # of ms to wait for an ack
+#define LED     9  // Moteinos have LEDs on D9
+
+RFM69_ATC radio;
+
+#define FLASH_SS      8 // and FLASH SS on D8 on regular Moteinos (D23 on MoteinoMEGA)
+SPIFlash flash(FLASH_SS, 0xEF30); //EF30 for 4mbit  Windbond chip (W25X40CL)
 
 VL53L0X sensor1;
 VL53L0X sensor2;
 
-/* Get MAC address as a char array */
-static const char* mac_address() {
-  static char macStr[18] = { 0 };
-  uint8_t mac[6];
-  WiFi.macAddress(mac);
-  sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  return macStr;
-}
-
-static const char* client_name = mac_address();
-
-/* Generate MQTT topic with the MAC address.
- *  Topic name will always be "smarter/presence/data/MACADDRESS"
- */
-static const char* mqtt_topic() {
-  static char topic[40];
-  sprintf(topic, "smarter/presence/data/%s", client_name);
-  return topic;
-}
-
-static const char* data_topic = mqtt_topic();
-
 void setup() {
+  pinMode(LED, OUTPUT);
   pinMode(xshut1, OUTPUT);
   pinMode(xshut2, OUTPUT);
   digitalWrite(xshut1, LOW);
   digitalWrite(xshut2, LOW);
 
-  Serial.begin (9600);
+  Serial.begin(SERIAL_BAUD);
 
-  setup_wifi();
-  mqtt_client.setServer(mqtt_host, mqtt_port);
+  if (flash.initialize()) flash.sleep(); //if Moteino has FLASH-MEM, make sure it sleeps
 
-  Wire.begin(4, 5);
+  Wire.begin();
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+  radio.encrypt(ENCRYPTKEY);
+  radio.enableAutoPower(ATC_RSSI);
 
   Serial.println("Initializing Sensor 1...");
   pinMode(xshut1, INPUT);
@@ -85,48 +81,7 @@ void setup() {
   sensor2.startContinuous();
 
   calibrate();
-}
-
-/* Connect to wifi */
-void setup_wifi() {
-  Serial.print("Connecting to ");
-  Serial.print(wifi_ssid);
-
-  WiFi.begin(wifi_ssid, wifi_password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(" . ");
-    delay(500);
-  }
-
-  Serial.print(WiFi.localIP());
-  Serial.println(" connected.");
-}
-
-/* Connect to MQTT broker */
-void setup_mqtt() {
-  // if wifi disconnected, reconnect
-  if (WiFi.status() != WL_CONNECTED) {
-    setup_wifi();
-  }
-
-  static char lifecycle_topic[45];
-  sprintf(lifecycle_topic, "smarter/presence/lifecycle/%s", client_name);
-
-  Serial.print("Connecting to ");
-  Serial.print(mqtt_host);
-
-  // loop until reconnected
-  while (!mqtt_client.connected()) {
-    Serial.print(" . ");
-    if (mqtt_client.connect(client_name, lifecycle_topic, 0, true, "disconnected")) {
-      Serial.print(client_name);
-      Serial.println(" connected.");
-      mqtt_client.publish(lifecycle_topic, "connected", true);
-    } else {
-      delay(1000);
-    }
-  }
+  radio.sendWithRetry(GATEWAYID, "START", 5);
 }
 
 void calibrate() {
@@ -196,10 +151,6 @@ uint8_t read_sensor2() {
 }
 
 void loop() {
-  if (!mqtt_client.loop()) {
-    setup_mqtt();
-  }
-
   static uint8_t _start = 0;
   static uint8_t _end = 0;
   static uint8_t confidence = 0;
@@ -210,6 +161,7 @@ void loop() {
   if (s1 == SENSOR_ERROR || s2 == SENSOR_ERROR) {
     // at least one of the readings is definitely wrong,
     // let's just skip this cycle and try again.
+    radio.sendWithRetry(GATEWAYID, "ERROR", 5);
     return;
   }
 
@@ -230,11 +182,11 @@ void loop() {
         int dir = _start - _end;
         if (dir == 1) {
           // moved from sensor 2 to sensor 1
-          mqtt_client.publish(data_topic, "2-1");
+          radio.sendWithRetry(GATEWAYID, "2-1", 3);
           Serial.println("---- 2-1 ----");
         } else if (dir == -1) {
           // moved from sensor 1 to sensor 2
-          mqtt_client.publish(data_topic, "1-2");
+          radio.sendWithRetry(GATEWAYID, "1-2", 3);
           Serial.println("---- 1-2 ----");
         }
       }
@@ -267,6 +219,7 @@ void loop() {
         }
       }
     }
+    Blink(LED,3);
   } else {
     confidence++;
     // there is activity on one side or the other
@@ -279,6 +232,18 @@ void loop() {
       // activity is ongoing, track where it might end.
       _end = (diff == 1 ? 1 : 2);
     }
+    Blink(LED,3);
+    radio.sendWithRetry(GATEWAYID, "READ", 4);
   }
+
+  radio.sleep();
+}
+
+void Blink(byte PIN, int DELAY_MS)
+{
+  pinMode(PIN, OUTPUT);
+  digitalWrite(PIN,HIGH);
+  delay(DELAY_MS);
+  digitalWrite(PIN,LOW);
 }
 
