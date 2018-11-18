ac76adb5359a9341b3f90a7c327aade142a6de86
Neil Gupta <ngupta@instructure.com>
2018-11-18 17:55:48 -0600

    track entry/exit counts and clean up macros

--- thermal.h
+++ thermal.h
@@ -11,6 +11,9 @@ Adafruit_AMG88xx amg;
 float calibrated_pixels[AMG88xx_PIXEL_ARRAY_SIZE];
 float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
 
+uint8_t past_left = 0;
+uint8_t past_right = 0;
+
 volatile boolean motion = false;
 void motionISR() {
   motion = true;
@@ -37,7 +40,6 @@ const uint8_t xcoordinates[64] = {
   1,  2,  3,  4,  5,  6,  7,  8,
   1,  2,  3,  4,  5,  6,  7,  8
 };
-
 const uint8_t ycoordinates[64] = {
   1,  1,  1,  1,  1,  1,  1,  1,
   2,  2,  2,  2,  2,  2,  2,  2,
@@ -86,19 +88,31 @@ void loop() {
     }
   }
 
-  #define NOT_LEFT_EDGE ( idx % GRID_EXTENT > 0 )
-  #define NOT_RIGHT_EDGE ( idx % GRID_EXTENT < GRID_EXTENT-1 )
-  #define MIDDLE_LEFT ( pixels[idx-1] > calibrated_pixels[idx-1] + TEMP_BUFFER )
-  #define TOP_LEFT ( idx > GRID_EXTENT && pixels[idx-(GRID_EXTENT+1)] > calibrated_pixels[idx-(GRID_EXTENT+1)] + TEMP_BUFFER )
-  #define BOTTOM_LEFT ( idx < (AMG88xx_PIXEL_ARRAY_SIZE - GRID_EXTENT) && pixels[idx+(GRID_EXTENT-1)] > calibrated_pixels[idx+(GRID_EXTENT-1)] + TEMP_BUFFER )
-  #define MIDDLE_RIGHT ( pixels[idx+1] > calibrated_pixels[idx+1] + TEMP_BUFFER )
-  #define TOP_RIGHT ( idx > (GRID_EXTENT-1) && pixels[idx-(GRID_EXTENT-1)] > calibrated_pixels[idx-(GRID_EXTENT-1)] + TEMP_BUFFER )
-  #define BOTTOM_RIGHT ( idx < (AMG88xx_PIXEL_ARRAY_SIZE - GRID_EXTENT) && pixels[idx+(GRID_EXTENT+1)] > calibrated_pixels[idx+(GRID_EXTENT+1)] + TEMP_BUFFER )
-  #define TOP_MIDDLE ( idx > (GRID_EXTENT-1) && pixels[idx-GRID_EXTENT] > calibrated_pixels[idx-GRID_EXTENT] + TEMP_BUFFER )
-  #define BOTTOM_MIDDLE ( idx < (AMG88xx_PIXEL_ARRAY_SIZE - GRID_EXTENT) && pixels[idx+GRID_EXTENT] > calibrated_pixels[idx+GRID_EXTENT] + TEMP_BUFFER )
+  // check temperatures in neighboring cells
+  #define NOT_LEFT_EDGE   ( xcoordinates[idx] > 1 )
+  #define NOT_RIGHT_EDGE  ( xcoordinates[idx] < GRID_EXTENT )
+  #define NOT_TOP_EDGE    ( idx >= GRID_EXTENT )
+  #define NOT_BOTTOM_EDGE ( idx < (AMG88xx_PIXEL_ARRAY_SIZE - GRID_EXTENT) )
+  #define TL_IDX ( idx - (GRID_EXTENT+1) )
+  #define TM_IDX ( idx - GRID_EXTENT )
+  #define TR_IDX ( idx - (GRID_EXTENT-1) )
+  #define ML_IDX ( idx - 1 )
+  #define MR_IDX ( idx + 1 )
+  #define BL_IDX ( idx + (GRID_EXTENT-1) )
+  #define BM_IDX ( idx + GRID_EXTENT )
+  #define BR_IDX ( idx + (GRID_EXTENT+1) )
+  #define CHECK_PIXEL(i)( pixels[(i)] > (calibrated_pixels[(i)] + TEMP_BUFFER) )
+  #define MIDDLE_LEFT   ( CHECK_PIXEL(ML_IDX) )
+  #define MIDDLE_RIGHT  ( CHECK_PIXEL(MR_IDX) )
+  #define TOP_LEFT      ( NOT_TOP_EDGE && CHECK_PIXEL(TL_IDX) )
+  #define TOP_MIDDLE    ( NOT_TOP_EDGE && CHECK_PIXEL(TM_IDX) )
+  #define TOP_RIGHT     ( NOT_TOP_EDGE && CHECK_PIXEL(TR_IDX) )
+  #define BOTTOM_LEFT   ( NOT_BOTTOM_EDGE && CHECK_PIXEL(BL_IDX) )
+  #define BOTTOM_MIDDLE ( NOT_BOTTOM_EDGE && CHECK_PIXEL(BM_IDX) )
+  #define BOTTOM_RIGHT  ( NOT_BOTTOM_EDGE && CHECK_PIXEL(BR_IDX) )
 
   uint8_t total_masses = 0;
-  uint8_t points[5];
+  uint8_t points[5]; // assuming we'll never see more than 5 people at once in a frame
   for(uint8_t i=0; i<count; i++){
     uint8_t idx = ordered_indexes[i];
     bool distinct = true;
@@ -109,34 +123,55 @@ void loop() {
       }
     }
     if (distinct && (
+      // has at least one neighboring cell (ignore lone temp readings)
       (NOT_LEFT_EDGE && (MIDDLE_LEFT || TOP_LEFT || BOTTOM_LEFT)) ||
       (NOT_RIGHT_EDGE && (MIDDLE_RIGHT || TOP_RIGHT || BOTTOM_RIGHT)) ||
       TOP_MIDDLE || BOTTOM_MIDDLE)) {
-        points[total_masses] = idx;
-        total_masses++;
-      }
+      points[total_masses] = idx;
+      total_masses++;
+    }
   }
 
-  if (total_masses > 0) {
-    for(int i = 0; i<total_masses; i++) {
-      Serial.print(points[i]);
-      Serial.print(", ");
+  uint8_t left_bucket = 0;
+  uint8_t right_bucket = 0;
+  for (int i=0; i<total_masses; i++) {
+    if (xcoordinates[points[i]] <= (GRID_EXTENT/2)) {
+      left_bucket++;
+    } else {
+      right_bucket++;
     }
-    Serial.println();
-
-    Serial.print("[");
-    for(int i=1; i<=AMG88xx_PIXEL_ARRAY_SIZE; i++){
-      float curr = pixels[i-1];
-      float last = calibrated_pixels[i-1];
-      if (curr - last > TEMP_BUFFER) {
-        Serial.print(curr);
-      } else
-        Serial.print("-----");
-      Serial.print(", ");
-      if( i%8 == 0 ) Serial.println();
-    }
-    Serial.println("]");
-    Serial.println();
   }
+
+  if (left_bucket > past_left && right_bucket < past_right) {
+    Sprint(left_bucket - past_left);
+    Sprintln(" entered");
+  }
+  if (right_bucket > past_right && left_bucket < past_left) {
+    Sprint(right_bucket - past_right);
+    Sprintln(" exited");
+  }
+
+  past_left = left_bucket;
+  past_right = right_bucket;
+
+//  for(int i = 0; i<total_masses; i++) {
+//    Serial.print(points[i]);
+//    Serial.print(", ");
+//  }
+//  Serial.println();
+//
+//  Serial.print("[");
+//  for(int i=1; i<=AMG88xx_PIXEL_ARRAY_SIZE; i++){
+//    float curr = pixels[i-1];
+//    float last = calibrated_pixels[i-1];
+//    if (curr - last > TEMP_BUFFER) {
+//      Serial.print(curr);
+//    } else
+//      Serial.print("-----");
+//    Serial.print(", ");
+//    if( i%8 == 0 ) Serial.println();
+//  }
+//  Serial.println("]");
+//  Serial.println();
 }
 
