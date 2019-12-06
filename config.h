7589c48e0af653a1833e718ca1d2256a4412b208
Neil Gupta <neil@hiome.com>
2019-12-05 20:22:46 -0600

    gitignore config

--- config.h
+++ config.h
@@ -1,12 +1,4 @@
-// Choose one:
-//#define LIDAR
-//#define MOTION
-//#define DOOR
+#define NODEID 25
 #define THERMAL
-//#define BED
-
-#define NODEID        5   // unique for each node on same network
-// WARNING MAKE SURE THIS IS SET CORRECTLY OR YOU CAN BRICK DEVICE
-//#define R3
-//#define RECESSED
-//#define ENABLE_SERIAL
+#define R3
+#define RECESSED
