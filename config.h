f49a6f399fc99b79f0dd9d27e083aa2b9b79d6f4
Neil Gupta <neil@hiome.com>
2019-12-05 20:38:50 -0600

    Revert "gitignore config"
    This reverts commit 7589c48e0af653a1833e718ca1d2256a4412b208.

--- config.h
+++ config.h
@@ -1,4 +1,12 @@
-#define NODEID 25
+// Choose one:
+//#define LIDAR
+//#define MOTION
+//#define DOOR
 #define THERMAL
-#define R3
-#define RECESSED
+//#define BED
+
+#define NODEID        5   // unique for each node on same network
+// WARNING MAKE SURE THIS IS SET CORRECTLY OR YOU CAN BRICK DEVICE
+//#define R3
+//#define RECESSED
+//#define ENABLE_SERIAL
