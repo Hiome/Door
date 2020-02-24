3750fc3f0340cd13fea50a43f89771c7356227be
Neil Gupta <neil@hiome.com>
2020-02-23 18:07:53 -0600

    bump to V20.2.24

--- thermal.h
+++ thermal.h
@@ -4,7 +4,7 @@
 //  #define TIME_CYCLES
 #endif
 
-#define FIRMWARE_VERSION        "V20.2.23"
+#define FIRMWARE_VERSION        "V20.2.24"
 #define YAXIS                        // axis along which we expect points to move (x or y)
 
 #include "thermal_types.h"
