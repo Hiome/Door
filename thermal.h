10a625badfbdea4d05408fdb5a33fdb00073475f
Neil Gupta <neil@hiome.com>
2020-04-08 13:21:09 -0500

    bump to V20.4.8b

--- thermal.h
+++ thermal.h
@@ -4,7 +4,7 @@
 //  #define TIME_CYCLES
 #endif
 
-#define FIRMWARE_VERSION        "V20.4.8"
+#define FIRMWARE_VERSION        "V20.4.8b"
 #define YAXIS                        // axis along which we expect points to move (x or y)
 
 #include "thermal/types.h"
