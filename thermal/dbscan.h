abbe6c82c5ab7f323e39088ecf4814dc7dd2b1ca
Neil Gupta <neil@hiome.com>
2020-04-14 12:47:02 -0500

    simplify dbscan to only consider temp diff from cluster head

--- thermal/dbscan.h
+++ thermal/dbscan.h
@@ -107,86 +107,40 @@ uint8_t findCurrentPoints() {
     float fgd = fgDiff(current_point);
     float bgd = bgDiff(current_point);
     float mt = maxTempDiffForTemps(fgd, bgd);
+    uint8_t blobSize = 1;
+    axis_t minAxis = AXIS(current_point);
+    axis_t maxAxis = minAxis;
+    axis_t minNAxis = NOT_AXIS(current_point);
+    axis_t maxNAxis = minNAxis;
+    uint8_t neighbors = 0;
 
     // scan all points added after current_point, since they must be part of same blob
     for (uint8_t x=sorted_size-1; x<sorted_size; x++) {
-      coord_t blobPoint = ordered_indexes_temp[x];
-      float mtb;
-      if (blobPoint == current_point) {
-        mtb = mt;
-      } else {
-        mtb = maxTempDiffForPoint(blobPoint);
-        // find how many neighbors this point has
-        // uint8_t fnc = 0;
-        // coord_t foundNeighbor = UNDEF_POINT;
-        // coord_t blobNeighbors[8];
-        // uint8_t nc = loadNeighbors(blobPoint, blobNeighbors);
-        // for (uint8_t bn = 0; bn < nc; bn++) {
-        //   if (clusterNum[blobNeighbors[bn]] == clusterIdx) {
-        //     fnc++;
-        //     if (fnc == 2) break;
-        //     foundNeighbor = blobNeighbors[bn];
-        //   }
-        // }
-        // if (fnc == 1) {
-        //   // if point only has 1 connection to this blob, maybe it's time to stop expanding
-        //   bool skippable = true;
-        //   for (uint8_t bn = 0; bn < nc; bn++) {
-        //     if (clusterNum[blobNeighbors[bn]] == 0 &&
-        //           norm_pixels[blobNeighbors[bn]] > CONFIDENCE_THRESHOLD &&
-        //           isNeighborly(blobNeighbors[bn], foundNeighbor) &&
-        //           diffFromPoint(blobNeighbors[bn], blobPoint) < mtb) {
-        //       // keep expanding if there are actually more connections to the blob
-        //       skippable = false;
-        //       break;
-        //     }
-        //   }
-        //   if (skippable) continue;
-        // }
-      }
-
       for (uint8_t k=y+1; k<active_pixel_count; k++) {
         // scan all known points after current_point to find neighbors to point x
         if (ordered_indexes[k] != UNDEF_POINT &&
-            isNeighborly(ordered_indexes[k], blobPoint) &&
-            (diffFromPoint(ordered_indexes[k], blobPoint) < mtb ||
-              diffFromPoint(ordered_indexes[k], current_point) < mt)) {
+            isNeighborly(ordered_indexes[k], ordered_indexes_temp[x]) &&
+              diffFromPoint(ordered_indexes[k], current_point) < mt) {
           clusterNum[ordered_indexes[k]] = clusterIdx;
           ordered_indexes_temp[sorted_size] = ordered_indexes[k];
           sorted_size++;
+
+          blobSize++;
+          axis_t axisn = AXIS(ordered_indexes[k]);
+          minAxis = min(minAxis, axisn);
+          maxAxis = max(maxAxis, axisn);
+          axis_t naxisn = NOT_AXIS(ordered_indexes[k]);
+          minNAxis = min(minNAxis, naxisn);
+          maxNAxis = max(maxNAxis, naxisn);
+          if (ordered_indexes_temp[x] == current_point) neighbors++;
+
           ordered_indexes[k] = UNDEF_POINT;
         }
       }
     }
 
     // check if point is too small/large to be a valid person
-    uint8_t blobSize = 1;
-    uint8_t totalBlobSize = 1;
-    axis_t minAxis = AXIS(current_point);
-    axis_t maxAxis = minAxis;
-    axis_t minNAxis = NOT_AXIS(current_point);
-    axis_t maxNAxis = minNAxis;
-    uint8_t neighbors = 0;
-    for (coord_t n = 0; n < AMG88xx_PIXEL_ARRAY_SIZE; n++) {
-      if (clusterNum[n] == clusterIdx && n != current_point) {
-        totalBlobSize++;
-
-        float dp = diffFromPoint(n, current_point);
-        if (dp > mt) continue;
-
-        blobSize++;
-        axis_t axisn = AXIS(n);
-        minAxis = min(minAxis, axisn);
-        maxAxis = max(maxAxis, axisn);
-        axis_t naxisn = NOT_AXIS(n);
-        minNAxis = min(minNAxis, naxisn);
-        maxNAxis = max(maxNAxis, naxisn);
-        if (isNeighborly(n, current_point)) neighbors++;
-      }
-    }
-
-    if (totalBlobSize > 50) {
-      // should never happen, but ensure there's no overflow
+    if (blobSize > 50) {
       SERIAL_PRINT(F("x "));
       SERIAL_PRINT(current_point);
       SERIAL_PRINTLN(F(" toobig"));
@@ -199,7 +153,7 @@ uint8_t findCurrentPoints() {
     uint8_t boundingBox = min(dimension, 5);
     // ignore a blob that fills less than 1/3 of its bounding box
     // a blob with 9 points will always pass density test
-    if ((totalBlobSize + min((uint8_t)fgd, (uint8_t)bgd))*3 >= sq(boundingBox)) {
+    if ((blobSize + min((uint8_t)fgd, (uint8_t)bgd))*3 >= sq(boundingBox)) {
       uint8_t noiseSize = 0;
       float mt_constrained = mt*0.7;
       mt_constrained = constrain(mt_constrained, 0.51, 1.51);
